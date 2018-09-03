49b82153a0d8537f347f346c38ca6caa8abb7186
Dongep <815115601@qq.com>
2018-09-03 23:51:23 +0800

    ...

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const koa = require('koa');
+const app = new koa();
+
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
