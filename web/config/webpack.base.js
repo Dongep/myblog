d292f66fa3b03a53bc3307a579620225172473e8
unknown <xn068605@xiaoniu.com>
2018-09-10 17:13:40 +0800

    update

--- web/config/webpack.base.js
+++ web/config/webpack.base.js
@@ -1,8 +1,37 @@
+const path = require('path')
 let present_path = process.cwd();
 function abs_path(rel_path){
     return path.resolve(present_path,rel_path)
 }
 
 module.exports={
-    entry: abs_path('../web/index.js')
+    entry: abs_path('../web/main.js'),
+    output: {
+        path: abs_path('../dist'),
+        filename: 'main.js',
+        publicPath: "/static",
+        chunkname: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js(x)?$/,
+                include: [abs_path('../src')],
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["es2015"]
+                          },
+                    },
+                    {
+                        loader: "eslint-loader",
+                        options: {
+                          },
+                    }
+                ]
+                
+            }
+        ]
+    }
 }
