5838fb89fe657ee624799602f4192576b0a371b9
dong <dong@dong.lan>
2018-10-16 23:01:06 +0800

    update

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './module/index/index';
-import Login from './module/login/login';
 
 let root = document.querySelector('#blog');
-let app=(
-    <index></index>
-)
-ReactDOM.render(Index,root)
+
+ReactDOM.render(<Index/>,root)
--- web/src/module/index/index.js
+++ web/src/module/index/index.js
@@ -3,24 +3,21 @@ import ReactDOM from 'react-dom';
 import Login from '../login/login';
 // import Content from '../content/content';
 
-class Container extends React.Component {
+export default class Container extends React.Component {
+    constructor() {
+        super()
+    }
     render() {
-        return 
-            (<div>
+        return (
+            <div>
                 <h1>奥利bama的博客</h1>
                 <header>奥利bama的博客</header>
                 <Login></Login>
                 {/* <Content></Content> */}
-            </div>)
-        
+            </div>
+        )
+
     }
-  
+
 }
-export default (
-    (<div>
-        <h1>奥利bama的博客</h1>
-        <header>奥利bama的博客</header>
-        <Login></Login>
-        {/* <Content></Content> */}
-    </div>)
-)
+
